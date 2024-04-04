59c3f34c3d28ce6bd922915f67d17ec65dc155db
sxxngwxn <sxxngw0n@gmail.com>
2024-04-04 18:46:18 +0900

    주석(comment)

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java1"); //hello java1을 출력합니다.
+        System.out.println("hello java2"); //hello java2을 출력합니다.
+
+        /*
+        System.out.println("hello java3"); //hello java3을 출력합니다.
+        System.out.println("hello java4"); //hello java4을 출력합니다.
+         */
+    }
+}
