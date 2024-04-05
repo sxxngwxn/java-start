4ecc7237d9d70ccf0dad48389e4ee651d3bde13e
sxxngwxn <sxxngw0n@gmail.com>
2024-04-05 21:22:58 +0900

    문제와 풀이

---
+++ src/operator/ex/OperationEx2.java
@@ -0,0 +1,13 @@
+package operator.ex;
+
+public class OperationEx2 {
+    public static void main(String[] args) {
+        double num1 = 1.5, num2 = 2.5, num3 = 3.5;
+
+        double sum = num1 + num2 + num3;
+        double avg = sum / 3;
+
+        System.out.println(sum);
+        System.out.println(avg);
+    }
+}
