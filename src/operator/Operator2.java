d3918ecb6f5d3f84116ef01fa75027e3add767e1
sxxngwxn <sxxngw0n@gmail.com>
2024-04-05 20:38:53 +0900

    문자열 더하기

---
+++ src/operator/Operator2.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+        String str = "a + b = ";
+        int b = 10;
+
+        System.out.println("가나다" + "라마바사");
+        System.out.println(str + "가나다");
+        System.out.println("가나다" + b);
+        System.out.println(str + b);
+    }
+}
