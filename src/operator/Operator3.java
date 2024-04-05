33899a9ec2ae4cf9f8718250472eee61bdf10f5b
sxxngwxn <sxxngw0n@gmail.com>
2024-04-05 20:47:06 +0900

    연산자 우선순위

---
+++ src/operator/Operator3.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class Operator3 {
+    public static void main(String[] args) {
+        /*
+        * 괄호 우선
+        * 곱, 나누기
+        * 더하기, 뺴기
+        * 대입
+        * */
+        int a = (1 + 2) * 3 * (2 + 3);
+
+        System.out.println(a);
+    }
+}
