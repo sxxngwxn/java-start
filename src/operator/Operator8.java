2ef48c3be86e69141cacaaff68da1a9df89db1c5
sxxngwxn <sxxngw0n@gmail.com>
2024-04-05 21:16:26 +0900

    대입 연산자

---
+++ src/operator/Operator8.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class Operator8 {
+    public static void main(String[] args) {
+        int a = 5;
+
+        a += 3;
+        a -= 3;
+        a *= 3;
+        a /= 3;
+        a %= 3;
+        System.out.println(a);
+    }
+}
