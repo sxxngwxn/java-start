aa31898cdc8151d4c1f607b1aa039cf222e67413
sxxngwxn <sxxngw0n@gmail.com>
2024-04-05 21:12:44 +0900

    논리 연산자

---
+++ src/operator/Operator7.java
@@ -0,0 +1,19 @@
+package operator;
+
+public class Operator7 {
+    public static void main(String[] args) {
+
+        System.out.println(true && true);
+        System.out.println(true && false);
+
+        System.out.println(true || false);
+        System.out.println(true || false);
+
+        System.out.println(!true);
+        System.out.println(!false);
+
+        int a = 15;
+
+        System.out.println(10 < a && a < 20);
+    }
+}
