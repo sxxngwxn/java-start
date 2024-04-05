82f9d30a8b2d1133c4a4c89f1c25e0cdfce9f1d5
sxxngwxn <sxxngw0n@gmail.com>
2024-04-05 20:12:25 +0900

    문제와 풀이

---
+++ src/variable/ex/VarEx3Question.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3Question {
+    public static void main(String[] args) {
+        long longVal = 1000000000L;
+        System.out.println("longVar = " + longVal);
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+    }
+}
