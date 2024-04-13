2752f6382fd5d0f288e93e57aae8e82dc707693d
sxxngwxn <sxxngw0n@gmail.com>
2024-04-13 16:30:24 +0900

    2차원 배열 - 리팩토링2

--- src/array/ArrayDi0.java
+++ src/array/ArrayDi0.java
@@ -4,15 +4,16 @@ public class ArrayDi0 {
     public static void main(String[] args) {
         int[][] arr = new int[2][3];
 
-        arr[0][0] = 1;
-        arr[0][1] = 2;
-        arr[0][2] = 3;
-        arr[1][0] = 4;
-        arr[1][1] = 5;
-        arr[1][2] = 6;
 
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 3; j++) {
+        int value = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length ; j++) {
+                arr[i][j] = ++value;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length ; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
