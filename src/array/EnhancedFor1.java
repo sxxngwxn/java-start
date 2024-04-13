b424397bef5bd3f24f13020f4fba4206554e1556
sxxngwxn <sxxngw0n@gmail.com>
2024-04-13 16:38:15 +0900

    향상된 for문

---
+++ src/array/EnhancedFor1.java
@@ -0,0 +1,16 @@
+package array;
+
+public class EnhancedFor1 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+
+        /**
+         * 향샹된 for문
+         * for-each (iter)
+         */
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
