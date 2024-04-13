62d1ff278ef6a9f90710380654bb5ede370e05db
sxxngwxn <sxxngw0n@gmail.com>
2024-04-13 15:37:59 +0900

    배열 리펙토링

--- src/array/Array1.java
+++ src/array/Array1.java
@@ -2,6 +2,10 @@ package array;
 
 public class Array1 {
     public static void main(String[] args) {
-        int[] students = new int[5]; //new int[5]의 주소값을 student가 참조
+        int[] students = {90, 80, 70, 60, 50};
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println((i + 1) + "번째 학생의 점수 : " + students[i]);
+        }
     }
 }
